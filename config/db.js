2a5d52b44fe59420c4db380d1f59077526a405e8
fannypil <fanysh3@gmail.com>
2025-07-01 20:55:59 +0300

    Setup MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// load environment variables
+dotenv.config();
+
+// connect to MongoDB
+const connectDB= async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected successfully");
+    } catch(err){
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1); 
+    }
+}
+
+// export the connection function
+module.exports = connectDB;
