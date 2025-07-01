4d93b1a8751c5d4eb1bc77fbffa5c5c321585603
fannypil <fanysh3@gmail.com>
2025-07-01 20:59:19 +0300

    JWT authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+//Middleware function to protect routes
+module.exports= function(req, res, next){
+    // Get token from the request header
+    const token = req.header('x-auth-token');
+
+    // Check if no token
+    if(!token){
+        return res.status(401).json({msg: 'No token, authorization denied'});
+    }
+    // Verify token
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user; 
+        next(); 
+    }catch(err){
+        // If verification fails (e.g., token is expired or invalid)
+        res.status(401).json({msg: 'Token is not valid'});
+    }
+}
